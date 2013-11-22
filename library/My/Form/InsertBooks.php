0d9f18c383bb6524133fb6ec2a5ef1be17005818
Galillei <bazilio.rodger@gmail.com>
2013-11-22 19:39:09 +0300

    22/11/13

--- library/My/Form/InsertBooks.php
+++ library/My/Form/InsertBooks.php
@@ -20,6 +20,7 @@ class My_Form_InsertBooks extends Zend_Form
         {
             $category->addMultiOption($c['idCategory'],$c['name']);
         }
+        $addCategory = new Zend_Form_Element_Button(array('name'=>'addCategory','id'=>'addCategory','label'=>'Add Category'));
         $name = new Zend_Form_Element_Textarea('Name');
         $name->setLabel('Name:')
             ->setOptions(array('rows'=>'3','cols'=>'30'))
@@ -27,11 +28,13 @@ class My_Form_InsertBooks extends Zend_Form
             ->addFilter('HtmlEntities')
             ->addFilter('StringTrim');
         $emt = new ZendX_JQuery_Form_Element_AutoComplete('ac');
-        $emt->setLabel('Autocomplete:');
-        $emt->setJQueryParams(array('source'=>'/admin/catalog/ajax/authors'))
+        $emt->setLabel('Author:');
+        $emt->setJQueryParams(array('source'=>'/admin/catalog/ajax/authors', 'minLength'=>'3'))
             ->setOptions(array('size'=>26))
             ->addFilter('HtmlEntities')
             ->addFilter('StringTrim');
+        $addAuthors = new Zend_Form_Element_Button(array('name'=>'add','id'=>'addAuthors','label'=>'Add author'));
+        $addAuthors->setName('add-Author');
         $idColumns = new Zend_Form_Element_Hidden('id_columns');
         $idColumns->addFilter('Alnum');
         $publisher = new Zend_Form_Element_Select('id_publisher');
@@ -44,6 +47,7 @@ class My_Form_InsertBooks extends Zend_Form
         {
             $publisher->addMultiOption($c['idPublisher'],$c['name']);
         }
+        $addPublisher = new Zend_Form_Element_Button(array('name'=>'addPublisher','id'=>'addPublisher','label'=>'Add Publisher'));
         $isbn = new Zend_Form_Element_Text('isbn');
         $isbn->setLabel('ISBN:')
             ->setOptions(array('size'=>26))
@@ -53,16 +57,64 @@ class My_Form_InsertBooks extends Zend_Form
             ->addFilter('HtmlEntities');
         $picture = new Zend_Form_Element_File('pictures/books');
         $picture->setLabel('Pictures');
-        $picture->addValidator('IsImageSize');
-
-
+        $picture->addValidator('IsImage');
+        $synopsis = new Zend_Form_Element_Text('synopsis');
+        $synopsis->setLabel('Synopsis:')
+            ->setOptions(array('size'=>26))
+            ->setRequired('true')
+            ->addValidator('Alnum')
+            ->addFilter('StringTrim')
+            ->addFilter('HtmlEntities');
+        $pagesCount = new Zend_Form_Element_Text('pageCount');
+        $pagesCount->setLabel('Count of pages:')
+            ->setOptions(array('size'=>26))
+            ->setRequired('true')
+            ->addValidator('Int')
+            ->addFilter('StringTrim')
+            ->addFilter('HtmlEntities');
+        $year = new Zend_Form_Element_Text('year');
+        $year->setLabel('Publication date')
+            ->setOptions(array('size'=>26))
+            ->addValidator('Digits')
+            ->addValidator('StringLength')
+            ->addFilter('StringTrim')
+            ->addFilter('HtmlEntities');
+        $datePicker = new ZendX_JQuery_Form_Element_DatePicker('date');
+        $datePicker->setLabel('Date:')
+            ->setJQueryParams(array('dateFormat'=>'dd-mm-yy'))
+            ->setOptions(array('size'=>26))
+            ->addValidator('Date')
+            ->setRequired(true);
+        $price = new Zend_Form_Element_Text('price');
+        $price->setLabel('Price:')
+            ->setOptions(array('size'=>26))
+            ->addValidator('Int')
+            ->addFilter('StringTrim')
+            ->addFilter('HtmlEntities');
+        $countBooks = new Zend_Form_Element_Text('countBooks');
+        $countBooks->setLabel('Count of Books')
+            ->setOptions(array('size'=>26))
+            ->setValue(0)
+            ->addValidator('Int')
+            ->addFilter('StringTrim')
+            ->addFilter('HtmlEntities');
         $this->addElement($category);
+        $this->addElement($addCategory);
         $this->addElement($name);
         $this->addElement($emt);
         $this->addElement($idColumns);
+        $this->addElement($addAuthors);
         $this->addElement($publisher);
+        $this->addElement($addPublisher);
         $this->addElement($isbn);
         $this->addElement($picture);
+        $this->addElement($synopsis);
+        $this->addElement($pagesCount);
+        $this->addElement($year);
+        $this->addElement($datePicker);
+        $this->addElement($price);
+        $this->addElement($countBooks);
+
 
     }
 
@@ -81,4 +133,4 @@ class My_Form_InsertBooks extends Zend_Form
         return $q;
     }
 
-} 
+}
---
+++ public/js/scripts.js
@@ -0,0 +1,11 @@
+/**
+ * Created by artsem on 11/21/13.
+ */
+$(document).ready(function(){
+$("#addAuthor-element").on('click',function(){
+    window.location = "/admin/catalog/add/author";
+});
+$("#addPublisher").on('click',function(){
+        window.location = "/admin/catalog/add/publisher";
+});
+})
